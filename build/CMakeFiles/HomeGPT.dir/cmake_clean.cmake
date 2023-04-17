377ebc8971bf884c463d6af2dc7555d17b763161
Chinmay Nagrale <30885075+MiB3Avenger@users.noreply.github.com>
2023-04-17 04:47:53 +0100

    Trying to resolve conflicts.

---
+++ build/CMakeFiles/HomeGPT.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HomeGPT.dir/src/main.cpp.obj"
+  "CMakeFiles/HomeGPT.dir/src/main.cpp.obj.d"
+  "HomeGPT.exe"
+  "HomeGPT.exe.manifest"
+  "HomeGPT.pdb"
+  "libHomeGPT.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HomeGPT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
